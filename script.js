efddb8be93dc9f29f27a174c5fd830227073f64f
scRaul <61263078+scRaul@users.noreply.github.com>
2022-04-27 13:18:35 -0700

    Updated

--- script.js
+++ script.js
@@ -13,8 +13,10 @@ var index = {};
 var projects = [];
 
 projects.push( new Project("sort","Sorting Algorithms Animated","https://scraul.github.io/SortingProject/","sort.png"));
+projects.push( new Project("connect","Connect","https://github.com/scRaul/Connect","connect.png"));
+projects.push( new Project("cg","About 3D Computer Graphics","https://hw2-computer-graphics-1.raulramirez7.repl.co/","cg.png"));
 projects.push( new Project("pong","Pong","https://gamedev-pong.raulramirez7.repl.co/","pong.png"));
-projects.push( new Project("solar","Solar System","https://scraul.github.io/SolarSystem/","SolarSystem.png"));
+projects.push( new Project("solar","Solar System","https://scraul.github.io/SolarSystem/","solarsystem.png"));
 projects.push( new Project("poke","Pokedex","https://pokemon-api.raulramirez7.repl.co/","Pokedex.png"));
 projects.push( new Project("points","Point Distribution","https://scraul.github.io/PointDistributions/","points.png"));
 projects.push( new Project("rest","Resturant","https://replit.com/@RaulRamirez7/RestuarantProject#index.js","rest.png"));
@@ -24,6 +26,8 @@ for(let i = 0; i < projects.length;i++)
     index[(projects[i].id)] = i;
 
 projects[index.sort].description = "A visual representation of how quicksort and insertion sort differ";
+projects[index.connect].description ="C++ terminal game, goal to connect user specified slots in a row.";
+projects[index.cg].description = "A very breif overview on making 3D graphics using webgl";
 projects[index.pong].description = "Recreation of pong using HTML5, CSS and JS";
 projects[index.solar].description = "Final Project for Computer Graphics Course";
 projects[index.poke].description = "Web app for seaching up pokemon by name, fetches data fromu pokeapi.co";
