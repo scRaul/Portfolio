f3955fe8aecc5fbaa2513d452393a9a53c89694f
scRaul <61263078+scRaul@users.noreply.github.com>
2022-04-27 14:04:06 -0700

    Added Games

--- script.js
+++ script.js
@@ -14,12 +14,14 @@ var projects = [];
 
 projects.push( new Project("sort","Sorting Algorithms Animated","https://scraul.github.io/SortingProject/","sort.png"));
 projects.push( new Project("connect","Connect","https://github.com/scRaul/Connect","connect.png"));
+projects.push( new Project("crossy","Crossy Shooting","https://github.com/scRaul/CrossyShooting","crossy.png"));
+projects.push( new Project("proj","Project3","https://github.com/Raul-Sc/project03","proj.png"));
 projects.push( new Project("cg","About 3D Computer Graphics","https://hw2-computer-graphics-1.raulramirez7.repl.co/","cg.png"));
-projects.push( new Project("pong","Pong","https://gamedev-pong.raulramirez7.repl.co/","pong.png"));
+projects.push( new Project("rest","Resturant","https://replit.com/@RaulRamirez7/RestuarantProject#index.js","rest.png"));
 projects.push( new Project("solar","Solar System","https://scraul.github.io/SolarSystem/","solarsystem.png"));
 projects.push( new Project("poke","Pokedex","https://pokemon-api.raulramirez7.repl.co/","Pokedex.png"));
 projects.push( new Project("points","Point Distribution","https://scraul.github.io/PointDistributions/","points.png"));
-projects.push( new Project("rest","Resturant","https://replit.com/@RaulRamirez7/RestuarantProject#index.js","rest.png"));
+projects.push( new Project("pong","Pong","https://gamedev-pong.raulramirez7.repl.co/","pong.png"));
 projects.push( new Project("tut","Game Dev. Tutorials","https://tutorialgamedev.raulramirez7.repl.co/","tut.png"));
 
 for(let i = 0; i < projects.length;i++)
@@ -27,6 +29,8 @@ for(let i = 0; i < projects.length;i++)
 
 projects[index.sort].description = "A visual representation of how quicksort and insertion sort differ";
 projects[index.connect].description ="C++ terminal game, goal to connect user specified slots in a row.";
+projects[index.crossy].description = "Unity built game, endless runner but with armed chickens";
+projects[index.proj].description = "Unreal built game, where you are a robot trying to reach a destination before your battery run out";
 projects[index.cg].description = "A very breif overview on making 3D graphics using webgl";
 projects[index.pong].description = "Recreation of pong using HTML5, CSS and JS";
 projects[index.solar].description = "Final Project for Computer Graphics Course";
